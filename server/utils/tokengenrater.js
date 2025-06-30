a75792f98a85d129b5f63a408e7266970dbbf0ce
Anita jaggi <164060438+anitajaggi@users.noreply.github.com>
2025-06-30 12:22:40 +0530

    Update tokengenrater.js

--- server/utils/tokengenrater.js
+++ server/utils/tokengenrater.js
@@ -4,12 +4,12 @@ const generateToken = (res, user) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true, // Always true in production
-    sameSite: "None", // ← This is crucial for cross-site cookie sharing
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-  });
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+});
   return token;
 };
 
